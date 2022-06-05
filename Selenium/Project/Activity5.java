28820339b8305c3d4609b6fae99973aa60f1262f
Manisha6666 <106366324+Manisha6666@users.noreply.github.com>
2022-06-05 18:42:58 +0530

    Add files via upload

---
+++ Selenium/Project/Activity5.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+public class Activity5 {
+    public static void main(String[] args)
+    {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://alchemy.hguy.co/lms/");
+        driver.findElement(By.linkText("My Account")).click();
+        String accountTitle = driver.getTitle();
+        Assert.assertEquals(accountTitle,"My Account – Alchemy LMS");
+        driver.quit();
+    }
+}
